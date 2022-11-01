25c9298940306334a1e79fcd921987f1c088325e
shotomorisaki <jojoto8845@icloud.com>
2022-10-31 18:39:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -67,88 +67,90 @@ function initMap() {
 
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
 
-function initialize(lat,lng) {
-  // Create the map.
-  const pyrmont = { lat: lat, lng: lng};
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: pyrmont,
-    zoom: 17,
-    mapId: "8d193001f940fde3",
-  });
-  // Create the places service.
-  service = new google.maps.places.PlacesService(map);
-  let getNextPage;
-  const moreButton = document.getElementById("more");
 
-  moreButton.onclick = function () {
-    moreButton.disabled = true;
-    if (getNextPage) {
-      getNextPage();
-    }
-  };
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(
+//     browserHasGeolocation
+//       ? "Error: The Geolocation service failed."
+//       : "Error: Your browser doesn't support geolocation."
+//   );
+//   infoWindow.open(map);
+// }
 
-  // Perform a nearby search.
-  service.nearbySearch(
-    { location: pyrmont, radius: 500, type: prompt("Enter your interest") },
-    (results, status, pagination) => {
-      if (status !== "OK" || !results) return;
+// function initialize(lat,lng) {
+//   // Create the map.
+//   const pyrmont = { lat: lat, lng: lng};
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: pyrmont,
+//     zoom: 17,
+//     mapId: "8d193001f940fde3",
+//   });
+//   // Create the places service.
+//   service = new google.maps.places.PlacesService(map);
+//   let getNextPage;
+//   const moreButton = document.getElementById("more");
 
-      addPlaces(results, map);
-      moreButton.disabled = !pagination || !pagination.hasNextPage;
-      if (pagination && pagination.hasNextPage) 
-      {
-        getNextPage = () => {
-          // Note: nextPage will call the same handler function as the initial call
-          pagination.nextPage();
-        };
-      }
-    }
-  );
-}
+//   moreButton.onclick = function () {
+//     moreButton.disabled = true;
+//     if (getNextPage) {
+//       getNextPage();
+//     }
+//   };
 
-function addPlaces(places, map) 
-{
-  const placesList = document.getElementById("places");
+//   // Perform a nearby search.
+//   service.nearbySearch(
+//     { location: pyrmont, radius: 500, type: prompt("Enter your interest") },
+//     (results, status, pagination) => {
+//       if (status !== "OK" || !results) return;
 
-  for (const place of places) 
-  {
-    if (place.geometry && place.geometry.location) 
-    {
-      const image = 
-      {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
+//       addPlaces(results, map);
+//       moreButton.disabled = !pagination || !pagination.hasNextPage;
+//       if (pagination && pagination.hasNextPage) 
+//       {
+//         getNextPage = () => {
+//           // Note: nextPage will call the same handler function as the initial call
+//           pagination.nextPage();
+//         };
+//       }
+//     }
+//   );
+// }
 
-      new google.maps.Marker(
-      {
-        map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location,
-      });
+// function addPlaces(places, map) 
+// {
+//   const placesList = document.getElementById("places");
 
-      const li = document.createElement("li");
+//   for (const place of places) 
+//   {
+//     if (place.geometry && place.geometry.location) 
+//     {
+//       const image = 
+//       {
+//         url: place.icon,
+//         size: new google.maps.Size(71, 71),
+//         origin: new google.maps.Point(0, 0),
+//         anchor: new google.maps.Point(17, 34),
+//         scaledSize: new google.maps.Size(25, 25),
+//       };
 
-      li.textContent = place.name;
-      placesList.appendChild(li);
-      li.addEventListener("click", () => {map.setCenter(place.geometry.location);});
-    }
-  }
-}
+//       new google.maps.Marker(
+//       {
+//         map,
+//         icon: image,
+//         title: place.name,
+//         position: place.geometry.location,
+//       });
+
+//       const li = document.createElement("li");
+
+//       li.textContent = place.name;
+//       placesList.appendChild(li);
+//       li.addEventListener("click", () => {map.setCenter(place.geometry.location);});
+//     }
+//   }
+// }
 
 
 
